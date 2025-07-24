10d9bfc3dac1f00c00b75be97fbd15758c809ea1
Walace Owili <owiliwalace@gmail.com>
2025-07-24 20:46:05 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
-// app/layout.tsx or app/root-layout.tsx
 import './globals.css';
 import { Toaster } from 'sonner';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Fame',
+  description: 'Fame',
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
